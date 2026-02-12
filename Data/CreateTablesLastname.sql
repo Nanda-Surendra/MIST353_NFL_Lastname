b459d76eaa27329d0d41a2843673767670a525c1
Nanda Surendra <Nanda.Surendra@mail.wvu.edu>
2026-02-12 12:45:35 -0500

    Updated Create and Insert tables

--- Data/CreateTablesLastname.sql
+++ Data/CreateTablesLastname.sql
@@ -1,1 +1,33 @@
+-- Create a database for NFL app
+-- use master; 
+
+-- CREATE DATABASE MIST353_NFL_RDB_Lastname;
+
+-- DROP database NFL_RDB_Lastname;
+
+use MIST353_NFL_RDB_Lastname;
+
 -- Create tables for first iteration
+go
+
+create TABLE ConferenceDivision ( 
+    ConferenceDivisionID INT identity(1,1) 
+        constraint PK_ConferenceDivision PRIMARY KEY,
+    Conference NVARCHAR(50) NOT NULL
+        constraint CK_ConferenceNames CHECK (Conference IN ('AFC', 'NFC')),
+    Division NVARCHAR(50) NOT NULL
+        constraint CK_DivisionNames CHECK (Division IN ('East', 'North', 'South', 'West')),
+);
+
+go
+
+create TABLE Team ( 
+    TeamID INT identity(1,1) 
+        constraint PK_Team PRIMARY KEY,
+    TeamName NVARCHAR(50) NOT NULL,
+    TeamCityState NVARCHAR(50) NOT NULL,
+    TeamColors NVARCHAR(50) NOT NULL,
+    ConferenceDivisionID INT NOT NULL
+        constraint FK_Team_ConferenceDivision FOREIGN KEY REFERENCES ConferenceDivision(ConferenceDivisionID)
+);
+
