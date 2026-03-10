2bf7cb49aa05e84e04fd8048a38c5670bad0e168
Nanda Surendra <Nanda.Surendra@mail.wvu.edu>
2026-03-10 12:47:05 -0400

    End of March 10 class

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -9,6 +9,6 @@ def get_db_connection():
     database = os.getenv("DB_NAME")
     username = os.getenv("DB_LOGIN")
     password = os.getenv("DB_PASSWORD")
-
+    #ODBC Driver 18 for SQL Server can ONLY be used in Synchronous mode
     connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
     return pyodbc.connect(connection_string)
