3b4d4cbcc42dff730b1ba6c07da6232df39a3b62
Nanda Surendra <Nanda.Surendra@mail.wvu.edu>
2026-04-08 22:21:46 -0400

    revised get teams in same conf, div as specified team for pymssql

--- API/get_teams_in_same_conference_division_as_specified_team.py
+++ API/get_teams_in_same_conference_division_as_specified_team.py
@@ -3,19 +3,17 @@ from get_db_connection import get_db_connection
 def get_teams_in_same_conference_division_as_specified_team(
         team_name: str
 ):
-    #with get_db_connection() as conn:
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute( "{call procGetTeamsInSameConferenceDivisionAsSpecifiedTeam(?)}", (team_name,))
+    cursor = conn.cursor(as_dict=True)
+    cursor.execute("EXEC procGetTeamsInSameConferenceDivisionAsSpecifiedTeam %s", (team_name,))
     rows = cursor.fetchall()
     conn.close()
 
-    #Convert pyodbc.Row objects to dicts
     results = [
         {
-            "TeamName": row.TeamName,
-            "Conference": row.Conference,
-            "Division": row.Division
+            "TeamName": row["TeamName"],
+            "Conference": row["Conference"],
+            "Division": row["Division"]
         }
         for row in rows
     ]
